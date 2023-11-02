a79b6e070e28ae1b937a92fbc04c4b03b0fd3f9c
Malte Donzelmann <malte.donzelmann@gmx.de>
2023-11-02 15:09:40 +0100

    refactor(file_parser): functions without implicit error checks

--- src/classloader/file_parser.rs
+++ src/classloader/file_parser.rs
@@ -1,10 +1,14 @@
 use std::ptr::null;
-
+use anyhow::Context;
+use nom::IResult;
+use nom::multi::length_count;
 use nom::bytes::complete::{
     tag,
     take
 };
+use nom::number::complete::be_u16;
 use super::ClassFile;
+use super::CpInfo;
 
 fn take2(mut content: &[u8]) ->  &[u8]{
     let result = take::<usize,&[u8], ()>(2usize)(content).unwrap();
@@ -12,11 +16,24 @@ fn take2(mut content: &[u8]) ->  &[u8]{
     return result.1;
 }
 
-fn parse(path_to_file: String) -> ClassFile {
+fn parse_constant_pool(current_content: &[u8])-> IResult<&[u8], CpInfo>{
+
+    todo!()
+}
+
+fn parse_class_file(current_content: &[u8])->IResult<&[u8],ClassFile>{
+    let current_content = tag(b"\xCA\xFE\xBA\xBE")(current_content)?.0;
+    let (current_content, minor_ver) = be_u16(current_content)?;
+    let (current_content, major_ver) = be_u16(current_content)?;
+    let (current_content, constant_pool) = length_count(be_u16, parse_constant_pool)(current_content)?;
+    todo!()
+}
+
+fn parse(path_to_file: String) -> anyhow::Result<ClassFile> {
     //read input and magic number
-    let content = std::fs::read(path_to_file).expect("File can not be read");
-    let mut current_content: &[u8] = &content;
-    current_content = tag("0xCAFEBABE")(current_content).expect("Not a valid class file").0;
-    let minor_ver = take2(current_content);
-    let major_ver = take2(current_content);
+    let content = std::fs::read(path_to_file).context("File can not be read")?;
+    match parse_class_file(&content) {
+        Ok((_,class_file))=> Ok(class_file),
+        Err(e)=> Err(e.to_owned().into()),
+    }
 }
