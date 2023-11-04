f4f86705fe8c0b88b80a0316d8abb61f1d6391ba
Malte Donzelmann <malte.donzelmann@gmx.de>
2023-11-04 17:19:05 +0100

    style(src): fixed formatting

--- src/classloader.rs
+++ src/classloader.rs
@@ -1,65 +1,64 @@
 pub mod file_parser;
 
-use enumflags2::{bitflags, BitFlags };
-
+use enumflags2::{bitflags, BitFlags};
 
 #[bitflags]
 #[derive(Clone, Copy)]
 #[repr(u16)]
-enum ClassAccessFlag{
-    Public=0x0001,
-    Final=0x0010,
-    Super=0x0020,
-    Interface=0x0200,
-    Abstract=0x0400,
-    Synthetic=0x1000,
-    Annotation=0x2000,
-    Enum=0x4000,
+enum ClassAccessFlag {
+    Public = 0x0001,
+    Final = 0x0010,
+    Super = 0x0020,
+    Interface = 0x0200,
+    Abstract = 0x0400,
+    Synthetic = 0x1000,
+    Annotation = 0x2000,
+    Enum = 0x4000,
 }
 
 #[bitflags]
 #[derive(Clone, Copy)]
 #[repr(u16)]
-enum FieldAccessFlag{
-    Public=0x0001,
-    Private=0x002,
-    Protected=0x004,
-    Static=0x008,
-    Final=0x0010,
-    Volatile=0x0040,
-    Transient=0x0080,
-    Synthetic=0x1000,
-    Enum=0x4000,
+enum FieldAccessFlag {
+    Public = 0x0001,
+    Private = 0x002,
+    Protected = 0x004,
+    Static = 0x008,
+    Final = 0x0010,
+    Volatile = 0x0040,
+    Transient = 0x0080,
+    Synthetic = 0x1000,
+    Enum = 0x4000,
 }
 
 #[bitflags]
 #[derive(Clone, Copy)]
 #[repr(u16)]
-enum MethodAccessFlag{
-    Public=0x0001,
-    Private=0x002,
-    Protected=0x004,
-    Static=0x008,
-    Final=0x0010,
-    Synchronized=0x0020,
-    Bridge=0x0040,
-    Vargs=0x0080,
-    Native=0x0100,
-    Abstract=0x0400,
-    Strict=0x0800,
-    Synthetic=0x1000,
+enum MethodAccessFlag {
+    Public = 0x0001,
+    Private = 0x002,
+    Protected = 0x004,
+    Static = 0x008,
+    Final = 0x0010,
+    Synchronized = 0x0020,
+    Bridge = 0x0040,
+    Vargs = 0x0080,
+    Native = 0x0100,
+    Abstract = 0x0400,
+    Strict = 0x0800,
+    Synthetic = 0x1000,
 }
 
-enum ReferenceKind{
-    GetField=1,
-    GetStatic=2,
-    PutField=3,
-    PutStatic=4,
-    InvokeVirtual=5,
-    InvokeStatic=6,
-    InvokeSpecial=7,
-    NewInvokeSpecial=8,
-    InvokeInterface=9,
+enum ReferenceKind {
+    GetField = 1,
+    GetStatic = 2,
+    PutField = 3,
+    PutStatic = 4,
+    InvokeVirtual = 5,
+    InvokeStatic = 6,
+    InvokeSpecial = 7,
+    NewInvokeSpecial = 8,
+    InvokeInterface = 9,
 }
 
 impl TryFrom<u8> for ReferenceKind {
@@ -67,63 +66,85 @@ impl TryFrom<u8> for ReferenceKind {
 
     fn try_from(kind: u8) -> Result<Self, Self::Error> {
         match kind {
-            1 =>Ok(ReferenceKind::GetField), 
-            2 =>Ok(ReferenceKind::GetStatic),
-            3 =>Ok(ReferenceKind::PutField),
-            4 =>Ok(ReferenceKind::PutStatic),
-            5 =>Ok(ReferenceKind::InvokeVirtual),
-            6 =>Ok(ReferenceKind::InvokeStatic),
-            7 =>Ok(ReferenceKind::InvokeSpecial),
-            8 =>Ok(ReferenceKind::NewInvokeSpecial),
-            9 =>Ok(ReferenceKind::InvokeInterface),
-            _ => Err("ReferenceKind must be between 1-9 (inclusive)!")
+            1 => Ok(ReferenceKind::GetField),
+            2 => Ok(ReferenceKind::GetStatic),
+            3 => Ok(ReferenceKind::PutField),
+            4 => Ok(ReferenceKind::PutStatic),
+            5 => Ok(ReferenceKind::InvokeVirtual),
+            6 => Ok(ReferenceKind::InvokeStatic),
+            7 => Ok(ReferenceKind::InvokeSpecial),
+            8 => Ok(ReferenceKind::NewInvokeSpecial),
+            9 => Ok(ReferenceKind::InvokeInterface),
+            _ => Err("ReferenceKind must be between 1-9 (inclusive)!"),
         }
     }
 }
 
-enum  CpInfo{
-    ClassInfo{name_index: u16},
-    FieldRefInfo{class_index: u16, name_and_type_index: u16},
-    MethodRefInfo{class_index: u16, name_and_type_index: u16},
-    InterfaceMethodRefInfo{class_index: u16, name_and_type_index: u16},
-    StringInfo{string_index:u16},
+enum CpInfo {
+    ClassInfo {
+        name_index: u16,
+    },
+    FieldRefInfo {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    MethodRefInfo {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    InterfaceMethodRefInfo {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    StringInfo {
+        string_index: u16,
+    },
     IntegerInfo(i32),
     FloatInfo(f32),
     LongInfo(i64),
     DoubleInfo(f64),
-    NameAndTypeInfo{namae_index: u16, descriptor_index: u16},
+    NameAndTypeInfo {
+        namae_index: u16,
+        descriptor_index: u16,
+    },
     UTF8INFO(String),
-    MethodHandleInfo{reference_kind: ReferenceKind, reference_index: u16},
-    MethodTypeInfo{descriptor_index: u16},
-    InvokeDynamicInfo{bootstrap_method_attr_index: u16, name_and_type_index: u16},
-
+    MethodHandleInfo {
+        reference_kind: ReferenceKind,
+        reference_index: u16,
+    },
+    MethodTypeInfo {
+        descriptor_index: u16,
+    },
+    InvokeDynamicInfo {
+        bootstrap_method_attr_index: u16,
+        name_and_type_index: u16,
+    },
 }
 
-struct AttributeInfo{
+struct AttributeInfo {
     attribute_name_index: u16,
     info: Vec<u8>,
 }
 
-struct MethodInfo{
-    access_flags: BitFlags::<MethodAccessFlag>,
+struct MethodInfo {
+    access_flags: BitFlags<MethodAccessFlag>,
     name_index: u16,
     descriptor_index: u16,
     attributes: Vec<AttributeInfo>,
 }
 
-struct FieldInfo{
-    access_flags: BitFlags::<FieldAccessFlag>,
+struct FieldInfo {
+    access_flags: BitFlags<FieldAccessFlag>,
     name_index: u16,
     descriptor_index: u16,
     attributes: Vec<AttributeInfo>,
-
 }
 
 struct ClassFile {
     minor_version: u16,
     major_version: u16,
-    cp_info :Vec<CpInfo>,
-    access_flags: BitFlags::<ClassAccessFlag>,
+    cp_info: Vec<CpInfo>,
+    access_flags: BitFlags<ClassAccessFlag>,
     this_class: u16,
     super_class: u16,
     interfaces_count: u16,
@@ -131,5 +152,5 @@ struct ClassFile {
     field_info: Vec<FieldInfo>,
     method_info: Vec<MethodInfo>,
     attributes_count: u16,
-    attributes: Vec<AttributeInfo>
+    attributes: Vec<AttributeInfo>,
 }
--- src/classloader/file_parser.rs
+++ src/classloader/file_parser.rs
@@ -1,115 +1,177 @@
+use super::CpInfo;
+use super::{ClassFile, ReferenceKind};
+use crate::classloader::ClassAccessFlag;
+use anyhow::Context;
+use enumflags2::BitFlags;
+use nom::bytes::complete::{tag, take};
 use nom::combinator::map_res;
 use nom::error::make_error;
-use strum::IntoEnumIterator;
+use nom::error::ErrorKind;
+use nom::multi::length_count;
+use nom::number::complete::{
+    be_f32, be_f64, be_i32, be_i64, be_u16, be_u32, be_u8,
+};
+use nom::IResult;
 use std::path::Path;
 use std::ptr::null;
-use anyhow::Context;
-use nom::IResult;
-use nom::multi::length_count;
-use nom::bytes::complete::{
-    tag,
-    take
-};
-use nom::number::complete::{be_u16, be_u8, be_u32, be_f32, be_f64, be_i64, be_i32};
-use crate::classloader::ClassAccessFlag;
-use enumflags2::BitFlags;
-use nom::error::ErrorKind;
-use super::{ClassFile, ReferenceKind};
-use super::CpInfo;
+use strum::IntoEnumIterator;
 
-fn take2(mut content: &[u8]) ->  &[u8]{
-    let result = take::<usize,&[u8], ()>(2usize)(content).unwrap();
+fn take2(mut content: &[u8]) -> &[u8] {
+    let result = take::<usize, &[u8], ()>(2usize)(content).unwrap();
     content = result.0;
     return result.1;
 }
 
-fn parse_constant_pool(current_content: &[u8])-> IResult<&[u8], CpInfo>{
+fn parse_constant_pool(current_content: &[u8]) -> IResult<&[u8], CpInfo> {
     let tag_content = current_content;
-    let (current_content,tag) = be_u8(current_content)?;
+    let (current_content, tag) = be_u8(current_content)?;
     match tag {
-        7 =>{
-            let (current_content, name_index) = be_u16(current_content)?; 
-            Ok((current_content, CpInfo::ClassInfo { name_index: (name_index) }))
+        7 => {
+            let (current_content, name_index) = be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::ClassInfo {
+                    name_index: (name_index),
+                },
+            ))
         },
-        9=>{
-            let (current_content, class_index) = be_u16(current_content)?; 
-            let (current_content, name_and_type_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::FieldRefInfo { class_index: (class_index), name_and_type_index: (name_and_type_index) }))
+        9 => {
+            let (current_content, class_index) = be_u16(current_content)?;
+            let (current_content, name_and_type_index) =
+                be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::FieldRefInfo {
+                    class_index: (class_index),
+                    name_and_type_index: (name_and_type_index),
+                },
+            ))
         },
-        10=> {
-            let (current_content, class_index) = be_u16(current_content)?; 
-            let (current_content, name_and_type_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::MethodRefInfo { class_index: (class_index), name_and_type_index: (name_and_type_index) }))
+        10 => {
+            let (current_content, class_index) = be_u16(current_content)?;
+            let (current_content, name_and_type_index) =
+                be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::MethodRefInfo {
+                    class_index: (class_index),
+                    name_and_type_index: (name_and_type_index),
+                },
+            ))
         },
-        11=> {
-            let (current_content, class_index) = be_u16(current_content)?; 
-            let (current_content, name_and_type_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::InterfaceMethodRefInfo { class_index: (class_index), name_and_type_index: (name_and_type_index) }))
+        11 => {
+            let (current_content, class_index) = be_u16(current_content)?;
+            let (current_content, name_and_type_index) =
+                be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::InterfaceMethodRefInfo {
+                    class_index: (class_index),
+                    name_and_type_index: (name_and_type_index),
+                },
+            ))
         },
-        8=>{
-            let (current_content, string_index) = be_u16(current_content)?; 
-            Ok((current_content, CpInfo::StringInfo { string_index: (string_index) }))
-        }
-        3=>{
+        8 => {
+            let (current_content, string_index) = be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::StringInfo {
+                    string_index: (string_index),
+                },
+            ))
+        },
+        3 => {
             let (current_content, int_value) = be_i32(current_content)?; //when the byteorder is not big_endian, this produces the wrong number
-            Ok((current_content, CpInfo::IntegerInfo( int_value )))
+            Ok((current_content, CpInfo::IntegerInfo(int_value)))
         },
-        4=>{
+        4 => {
             let (current_content, float_value) = be_f32(current_content)?; //when the byteorder is not big_endian, this produces the wrong number
-            Ok((current_content, CpInfo::FloatInfo( float_value )))
+            Ok((current_content, CpInfo::FloatInfo(float_value)))
         },
-        5=>{
+        5 => {
             let (current_content, long_value) = be_i64(current_content)?; //when the byteorder is not big_endian, this produces the wrong number
-            Ok((current_content, CpInfo::LongInfo( long_value )))
+            Ok((current_content, CpInfo::LongInfo(long_value)))
         },
-        6=>{
+        6 => {
             let (current_content, float_value) = be_f64(current_content)?; //when the byteorder is not big_endian, this produces the wrong number
-            Ok((current_content, CpInfo::DoubleInfo( float_value )))
+            Ok((current_content, CpInfo::DoubleInfo(float_value)))
         },
-        12=>{
+        12 => {
             let (current_content, name_index) = be_u16(current_content)?;
             let (current_content, descriptor_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::NameAndTypeInfo { namae_index: (name_index), descriptor_index: (descriptor_index) }))
+            Ok((
+                current_content,
+                CpInfo::NameAndTypeInfo {
+                    namae_index: (name_index),
+                    descriptor_index: (descriptor_index),
+                },
+            ))
         },
-        1=>{
+        1 => {
             let (current_content, length) = be_u16(current_content)?;
             todo!()
-        }
-        15=>{
+        },
+        15 => {
             let (current_content, reference_kind) = be_u8(current_content)?;
             let (current_content, reference_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::MethodHandleInfo { reference_kind: (ReferenceKind::try_from(reference_kind).unwrap()), reference_index: (reference_index) }))
+            Ok((
+                current_content,
+                CpInfo::MethodHandleInfo {
+                    reference_kind: (ReferenceKind::try_from(reference_kind)
+                        .unwrap()),
+                    reference_index: (reference_index),
+                },
+            ))
         },
-        16=>{
+        16 => {
             let (current_content, descriptor_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::MethodTypeInfo { descriptor_index: (descriptor_index) }))
+            Ok((
+                current_content,
+                CpInfo::MethodTypeInfo {
+                    descriptor_index: (descriptor_index),
+                },
+            ))
         },
-        18=>{
-            let (current_content, bootstrap_method_attr_index) = be_u16(current_content)?;
-            let (current_content, name_and_type_index) = be_u16(current_content)?;
-            Ok((current_content, CpInfo::InvokeDynamicInfo { bootstrap_method_attr_index: (bootstrap_method_attr_index), name_and_type_index: (name_and_type_index) }))
+        18 => {
+            let (current_content, bootstrap_method_attr_index) =
+                be_u16(current_content)?;
+            let (current_content, name_and_type_index) =
+                be_u16(current_content)?;
+            Ok((
+                current_content,
+                CpInfo::InvokeDynamicInfo {
+                    bootstrap_method_attr_index: (bootstrap_method_attr_index),
+                    name_and_type_index: (name_and_type_index),
+                },
+            ))
         },
-        _ => Err(nom::Err::Failure(nom::error::Error::new(tag_content, ErrorKind::Tag)))
+        _ => Err(nom::Err::Failure(nom::error::Error::new(
+            tag_content,
+            ErrorKind::Tag,
+        ))),
     }
 }
 
-fn parse_class_file(current_content: &[u8])->IResult<&[u8],ClassFile>{
+fn parse_class_file(current_content: &[u8]) -> IResult<&[u8], ClassFile> {
     let current_content = tag(b"\xCA\xFE\xBA\xBE")(current_content)?.0;
     let (current_content, minor_ver) = be_u16(current_content)?;
     let (current_content, major_ver) = be_u16(current_content)?;
-    let (current_content, constant_pool) = length_count(be_u16, parse_constant_pool)(current_content)?;
+    let (current_content, constant_pool) =
+        length_count(be_u16, parse_constant_pool)(current_content)?;
     let (current_content, access_flag_byte) = be_u16(current_content)?;
-    let class_access_flag = BitFlags::<ClassAccessFlag>::from_bits(access_flag_byte);
-    let (current_content, super_class) = be_u16(current_content)?; 
-    
+    let class_access_flag =
+        BitFlags::<ClassAccessFlag>::from_bits(access_flag_byte);
+    let (current_content, super_class) = be_u16(current_content)?;
+
     todo!()
 }
 
 fn parse<P: AsRef<Path>>(path_to_file: P) -> anyhow::Result<ClassFile> {
     //read input and magic number
-    let content = std::fs::read(path_to_file).context("File can not be read")?;
+    let content =
+        std::fs::read(path_to_file).context("File can not be read")?;
     match parse_class_file(&content) {
-        Ok((_,class_file))=> Ok(class_file),
-        Err(e)=> Err(e.to_owned().into()),
+        Ok((_, class_file)) => Ok(class_file),
+        Err(e) => Err(e.to_owned().into()),
     }
 }
