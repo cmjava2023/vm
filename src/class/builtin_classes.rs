c57a64590d07d07d9dd4e9d5fc4ba4423d2c8321
Julius Bierbaum <julius.bierbaum1@hsbi.de>
2023-11-06 19:02:29 +0100

    feat: implement of builtin class for 'System'

--- src/class/builtin_classes.rs
+++ src/class/builtin_classes.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use crate::class::{Class, Method};
+use crate::class::{Class, Field, FieldValue, Method};
 
 #[deprecated(note = "use struct from executer in the future")]
 pub struct Frame {}
@@ -18,7 +18,7 @@ fn println(frame: &Frame) -> Update {
 }
 
 impl Class for PrintStream {
-    fn methods(&self) -> &[super::Method] {
+    fn methods(&self) -> &[Method] {
         &[Method::Rust(println)]
     }
 
@@ -46,3 +46,52 @@ impl Class for PrintStream {
         &[]
     }
 }
+
+pub struct SystemClass {
+    fields: Vec<Field>,
+}
+
+impl SystemClass {
+    pub fn new() -> Self {
+        let mut fields = Vec::new();
+        fields.push(Field {
+            name: "out".into(),
+            value: FieldValue::Reference(None),
+        });
+        Self { fields }
+    }
+}
+
+impl Class for SystemClass {
+    fn methods(&self) -> &[Method] {
+        &[]
+    }
+
+    fn static_fields(&self) -> &[Field] {
+        self.fields.as_slice()
+    }
+
+    fn instance_fields(&self) -> &[String] {
+        &[]
+    }
+
+    fn package(&self) -> &str {
+        "java/lang"
+    }
+
+    fn name(&self) -> &str {
+        "System"
+    }
+
+    fn super_class(&self) -> Option<Rc<dyn Class>> {
+        None
+    }
+
+    fn interfaces(&self) -> &[Rc<dyn std::any::Any>] {
+        &[]
+    }
+}
+
+pub struct ObjectClass {}
+
+pub struct StringClass {}
