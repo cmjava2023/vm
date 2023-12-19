163699cad6ddcef269fc98d8377923ab4b92c77c
Julius Bierbaum <julius.bierbaum1@hsbi.de>
2023-12-19 14:00:59 +0100

    style(input-tests): implicitly get byte representation of char

--- tests/input.rs
+++ tests/input.rs
@@ -4,12 +4,13 @@ use predicates::prelude::predicate;
 #[test]
 fn input() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("cmjava")?;
+    let sample_input = 'A';
 
     cmd.arg("tests/data/input/Main.class");
-    cmd.write_stdin("A");
+    cmd.write_stdin(format!("{}", sample_input));
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("65\n"));
+        .stdout(predicate::str::contains(format!("{}\n", sample_input as u8)));
 
     Ok(())
 }
