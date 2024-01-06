247ca5df4f21155f9f88e6c1e7a60accc420e555
Florian Warzecha <florian_filip.warzecha@hsbi.de>
2024-01-06 18:06:02 +0100

    feat(executor): op code {Get,Put}Field

--- tests/instance_function.rs
+++ tests/instance_function.rs
@@ -6,15 +6,9 @@ fn instance_function() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("cmjava")?;
 
     cmd.arg("tests/data/instance_function/Main.class");
-    // contains both an assert.failure() and an assert.success(),
-    // so that failure() can be simply removed when all features
-    // are implemented
-    cmd.assert().failure().stderr(predicate::str::contains(
-        "Missing OpCode implementation for: PutField {",
-    ));
-    // cmd.assert()
-    //     .success()
-    //     .stdout(predicate::str::contains("10\n"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("10\n"));
 
     Ok(())
 }
